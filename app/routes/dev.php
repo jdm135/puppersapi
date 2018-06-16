15d689922f3ac47e53ad2be14f2f17c0d5e9c6bd
jdm555 <johnmarcusdev@gmail.com>
2018-06-15 18:12:05 -0700

    Initial commit. Setup simple examples.

--- app/routes/dev.php
+++ app/routes/dev.php
@@ -1,10 +1,38 @@
 <?php
 
 use Puppers\User;
+use Puppers\Pup;
 
 $app->get('/dev', function($req, $res, $args){
 	$users = User::all();
 	return $res->withStatus(200)->withJson($users);
+});
+
+$app->get('/pup', function($req, $res, $args){
+    $pup = Pup::all();
+    return $res->withStatus(200)->withJson($pup);
+});
+
+$app->get('/pup/{pup_id}', function($req, $res, $args){
+    $id = $args['pup_id'];
+	$pup = Pup::find($id);
+    return $res->withStatus(200)->withJson($pup);
+});
+
+$app->post('/add-pup', function($req, $res, $args){
+    $post = $req->getParsedBody();
+    $addpup = Pup::create($post);
+});
+
+$app->post('/update-pup', function($req, $res, $args){
+    $post = $req->getParsedBody();
+    $updatepup = Pup::where('name', $post['name']);
+    //->update(['name' => $post['new-name'], 'age' => $post['age'], 'breed' => $post['breed']]);
+});
+
+$app->post('/delete-pup', function($req, $res, $args){
+    $post = $req->getParsedBody();
+    $deletepup = Pup::where('name', $post['name'])->delete();
 });
 
 ?>
